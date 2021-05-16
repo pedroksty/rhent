4f98fb52902a395391eb9250390197fd16424f52
pedroksty <pedrotrab2132@gmail.com>
2021-05-16 11:47:30 -0300

    Feature: Create initial api documentation

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
 import express from 'express'
+import swaggerUI from 'swagger-ui-express'
 import cors from 'cors'
+
+import swaggerFile from './swagger.json'
 import { router } from './routes'
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerFile))
 
 app.use(router)
 
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -7,4 +7,8 @@ const router = Router()
 router.use('/categories', categoriesRouter)
 router.use('/specifications', specifcationsRoutes)
 
+router.get('/', (request, response) => {
+  return response.json({ SERVER: 'ON' })
+})
+
 export { router }
